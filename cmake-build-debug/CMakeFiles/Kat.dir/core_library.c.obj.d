38ac80160a9bffa2a2066cd63aa5c992f7f4bca1
FinochioM <matias.finochio@davinci.edu.ar>
2024-06-27 12:21:54 -0300

    Merged core library with master branch

--- cmake-build-debug/CMakeFiles/Kat.dir/core_library.c.obj.d
+++ cmake-build-debug/CMakeFiles/Kat.dir/core_library.c.obj.d
@@ -1,39 +1,45 @@
-CMakeFiles/Kat.dir/vm.c.obj: \
- C:\Users\Matias\Downloads\Kat_in_C-master1\vm.c \
- D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdarg.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/stdarg.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_stdarg.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+CMakeFiles/Kat.dir/core_library.c.obj: \
+ C:\Users\Matias\Downloads\Kat_in_C-master1\core_library.c \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/math.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/ctype.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/string.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
- D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/stdlib.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/limits.h \
+ D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/syslimits.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/limits.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/malloc.h \
+ D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/errno.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/time.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sys/timeb.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/_timeval.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/pthread_time.h \
+ C:\Users\Matias\Downloads\Kat_in_C-master1\core_library.h \
+ C:\Users\Matias\Downloads\Kat_in_C-master1\vm.h \
+ C:\Users\Matias\Downloads\Kat_in_C-master1\object.h \
  C:\Users\Matias\Downloads\Kat_in_C-master1\common.h \
  D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdbool.h \
  D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stddef.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/stddef.h \
  D:/CLion\ 2024.1.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdint.h \
  D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/stdint.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\compiler.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\object.h \
  C:\Users\Matias\Downloads\Kat_in_C-master1\chunk.h \
  C:\Users\Matias\Downloads\Kat_in_C-master1\value.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/string.h \
+ D:/CLion\ 2024.1.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h \
  C:\Users\Matias\Downloads\Kat_in_C-master1\table.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\vm.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\debug.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\memory.h \
- C:\Users\Matias\Downloads\Kat_in_C-master1\core_library.h
+ C:\Users\Matias\Downloads\Kat_in_C-master1\memory.h
